66c2bfbd96d6487312a8bf9cd02000748bd74f3e
leonid_shuliak <zeleny777@gmail.com>
2023-04-27 10:35:31 +0300

    add base config to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import configuration from './configuration/configuration';
+const modules = [
+  ConfigModule.forRoot({
+    isGlobal: true,
+    load: [configuration],
+  }),
+];
 @Module({
-  imports: [],
+  imports: modules,
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+//import { appConfig } from './configuration/app.config';
+// import getLogLevels from './providers/logger/getLogLevels';
+import { ConfigurationType } from './configuration/configuration';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+    // logger: getLogLevels(false), // true - for production
+  });
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-ignore
+
+  const configService = app.get(ConfigService<ConfigurationType, true>);
+  const apiSettings = configService.get('apiSettings', { infer: true });
+  const environmentSettings = configService.get('environmentSettings', {
+    infer: true,
+  });
+  const PORT = apiSettings.PORT;
+
+  app.setGlobalPrefix('api');
+  // const createdApp = appConfig(app);
+  // swaggerConfig(createdApp);
+
+  await app.listen(PORT || 5001).then(async () => {
+    console.log('ENV', environmentSettings.currentEnv);
+    console.log(`Server is listening on ${PORT}`);
+  });
+  //connect to ngrok for development
+  // await connectToNgrok(createdApp);
 }
 bootstrap();
